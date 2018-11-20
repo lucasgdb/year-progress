ffa34f4b4f259ea9ff35bdd00867a050c9fede60
Lucas Naja <lucasnaja0@gmail.com>
2018-11-20 12:26:55 -0200

    some improvements

--- src/scripts.js
+++ src/scripts.js
@@ -46,20 +46,12 @@ function updateProgress() {
   progressBar[2].style.width = `${dayProgress}%`;
 
   workDayProgress = (() => {
-    if (date.getHours() < 7 || date.getHours() >= 23) {
-      dayName.textContent = 'Night';
-      return (date.getHours() - (date.getHours() === 23 ? 23 : -1) + date.getMinutes() / 60 + date.getSeconds() / 3600) / 8 * 100
-    } else if (date.getHours() < 9) {
-      dayName.textContent = 'Morning';
-      return (date.getHours() - 7 + date.getMinutes() / 60 + date.getSeconds() / 3600) / 2 * 100
-    } else if (date.getHours() < 18) {
-      dayName.textContent = 'Workday';
-      return (date.getHours() - 9 + date.getMinutes() / 60 + date.getSeconds() / 3600) / 9 * 100
-    } else {
-      dayName.textContent = 'Evening';
-      return (date.getHours() - 18 + date.getMinutes() / 60 + date.getSeconds() / 3600) / 5 * 100
-    }
-
+    let num = 0;
+    if (date.getHours() < 6) dayName.textContent = 'Dawn';
+    else if (date.getHours() < 12) { dayName.textContent = 'Morning'; num = 6 }
+    else if (date.getHours() < 18) { dayName.textContent = 'Evening'; num = 12 }
+    else { dayName.textContent = 'Night'; num = 18 }
+    return (date.getHours() - num + date.getMinutes() / 60 + date.getSeconds() / 3600) / 6 * 100
   })();
 
   workDay.textContent = `${toFixed(workDayProgress)}%`;
@@ -69,7 +61,7 @@ function updateProgress() {
 
 function toFixed(number) {
   let num = number.toString().substring(0, number.toString().indexOf('.') + 3);
-  return num.length !== 5 ? number.toFixed(2) : num;
+  return (num.length !== 5 || num.length !== 4) ? number.toFixed(2) : num;
 }
 
 window.onload = function() {
